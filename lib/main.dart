b3a82f59b96344d80c66b0e46bf2ef170847b81b
sonagidevelop <jinh98326@gmail.com>
2021-08-24 12:14:40 +0900

    psuuh
    a

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(widget.lat),
             FutureBuilder(
               future: _naver,
               builder: (context, snapshot) {

a64030be12e26d8a713c5169ef4851cf9ee4088c
sonagidevelop <jinh98326@gmail.com>
2021-08-24 14:32:02 +0900

    aa
    aa

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+List bgColors = [
+  [Color(0xff7faeff),Color(0xffe0bfff),Color(0xffffa67f),Color(0xffff6a7a),],
+  [Colors.red,Colors.black,Colors.white,Colors.deepPurple]
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:future_exercise/apis/sunsettimeapi.dart';
+import 'package:future_exercise/colors.dart';
 import 'package:future_exercise/init.dart';
 import 'package:future_exercise/apis/naverapi.dart';
 import 'package:future_exercise/splash.dart';
@@ -28,21 +29,11 @@ class MyApp extends StatelessWidget {
               String lati =
                   snapshot.data.toString().split(",")[1].substring(0, 10);
               return Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                        Color(0xff7faeff),
-                        Color(0xffe0bfff),
-                        Color(0xffffa67f),
-                        Color(0xffff6a7a),
-                      ])),
                   child: MyHomePage(
-                    title: lati,
-                    lon: long,
-                    lat: lati,
-                  ));
+                title: lati,
+                lon: long,
+                lat: lati,
+              ));
             } else {
               return SplashScreen();
             }
@@ -67,83 +58,113 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var width = screenSize.width;
+    var height = screenSize.height;
     final Future _naver = NaverApi.getNaverData(widget.lon, widget.lat);
     final Future _sunset =
         SunsetTimeApi.getSunsetTimeData(widget.lon, widget.lat);
 
     return Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FutureBuilder(
-              future: _naver,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  int lastindex =
-                      snapshot.data.toString().split(",")[1].indexOf(']');
-                  String shi =
-                      snapshot.data.toString().split(",")[0].substring(1);
-                  String gu = snapshot.data
-                      .toString()
-                      .split(",")[1]
-                      .substring(1, lastindex);
-                  return Container(
-                    child: Column(
-                      children: [
-                        Material(
-                          color: Colors.red.withOpacity(0),
-                          child: Text(
-                            shi,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.normal,
-                                textBaseline: TextBaseline.alphabetic),
-                          ),
-                        ),
-                        Text(gu)
-                      ],
-                    ),
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
-            ),
-            FutureBuilder(
-              future: _sunset,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  String aa = snapshot.data.toString().substring(1, 5);
-                  String fs = aa.substring(0, 2);
-                  String bs = aa.substring(2);
-                  String fd = DateFormat('yyyy-MM-dd').format(DateTime.now());
-                  var leftSstTimeStr = fd + ' ' + fs + ':' + bs;
-                  DateTime leftSstTime =
-                      new DateFormat('yyyy-MM-dd HH:mm').parse(leftSstTimeStr);
-                  Duration diff = leftSstTime.difference(DateTime.now());
-                  int leftMin = diff.inMinutes.toInt() + 1;
-                  String getLeftTimeStr(int value) {
-                    final int hour = value ~/ 60;
-                    final int minutes = value % 60;
-                    return '노을까지 ${hour.toString().padLeft(2, "0")}시간 ${minutes.toString().padLeft(2, "0")}분 남았습니다.';
-                  }
+      child: FutureBuilder(
+        future: _sunset,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            String aa = snapshot.data.toString().substring(1, 5);
+            String fs = aa.substring(0, 2);
+            String bs = aa.substring(2);
+            String fd = DateFormat('yyyy-MM-dd').format(DateTime.now());
+            var leftSstTimeStr = fd + ' ' + fs + ':' + bs;
+            DateTime leftSstTime =
+                new DateFormat('yyyy-MM-dd HH:mm').parse(leftSstTimeStr);
+            Duration diff = leftSstTime.difference(DateTime.now());
+            int leftMin = diff.inMinutes.toInt() + 1;
+            print(leftMin);
+            String getLeftTimeStr(int value) {
+              final int hour = value ~/ 60;
+              final int minutes = value % 60;
+              return '노을까지 ${hour.toString().padLeft(2, "0")}시간 ${minutes.toString().padLeft(2, "0")}분 남았습니다.';
+            }
 
-                  String finalTime = getLeftTimeStr(leftMin);
-                  String gong = "a";
-                  return Column(
+            String finalTime = getLeftTimeStr(leftMin);
+            String gong = "a";
+            return Scaffold(
+              body: Container(
+                  height: height,
+                  width: width,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                        if (leftMin > 120) ...[
+                          bgColors[0][0],
+                          bgColors[0][1],
+                          bgColors[0][2],
+                          bgColors[0][3],
+                        ] else if (leftMin > 30 && leftMin <= 120) ...[
+                          bgColors[1][0],
+                          bgColors[1][1],
+                          bgColors[1][2],
+                          bgColors[1][3]
+                        ]
+                      ]
+                          // colors: leftMin > 30 ? bgColors[0] : bgColors[1]
+                          )),
+                  child: Column(
                     children: [
-                      Text(aa),
-                      Text(finalTime),
+                      SafeArea(
+                        child: Column(
+                          children: [
+                            Text(aa),
+                            Text(finalTime),
+                            FutureBuilder(
+                              future: _naver,
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState ==
+                                    ConnectionState.done) {
+                                  int lastindex = snapshot.data
+                                      .toString()
+                                      .split(",")[1]
+                                      .indexOf(']');
+                                  String shi = snapshot.data
+                                      .toString()
+                                      .split(",")[0]
+                                      .substring(1);
+                                  String gu = snapshot.data
+                                      .toString()
+                                      .split(",")[1]
+                                      .substring(1, lastindex);
+                                  return Container(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          shi,
+                                        ),
+                                        Text(" "),
+                                        Text(gu)
+                                      ],
+                                    ),
+                                  );
+                                } else {
+                                  return Text("loading");
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned.fill(
+                          child: Image.asset('images/city1.png',))
                     ],
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
-            ),
-          ],
-        ),
+                  )),
+            );
+          } else {
+            return Scaffold(body: Center(child: Text("loading")));
+          }
+        },
       ),
     );
   }
